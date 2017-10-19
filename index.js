400f6780d107d6a06ceb8695581b570ea145799c
fogine <7884288+fogine@users.noreply.github.com>
2017-10-19 18:00:30 +0200

    refactoring, ask for all information needed in interactive mode

--- index.js
+++ index.js
@@ -13,29 +13,7 @@ function cliInterface(yargs, strict) {
     return yargs
     .usage('$0 <command> [options]')
     .command(['init'], 'Initializes a new bi-service project', {
-        plugin: {
-            alias: 'p',
-            describe: 'List of plugins which will be installed',
-            enum: Object.keys(plugins),
-            default: [],
-            array: true,
-            type: 'string'
-        },
-        app: {
-            alias: 'a',
-            describe: 'List of app names which will be created',
-            default: [],
-            array: true,
-            type: 'string'
-        },
     }, template.initCmd.bind(template))
-    .option('interactive', {
-        alias: 'i',
-        describe: 'if not enabled, it will NOT prompt the user for anything.',
-        default: true,
-        global: true,
-        type: 'boolean'
-    })
     .option('verbose', {
         alias: 'v',
         describe: 'Dumps more info to stdout',
